f94bdf9f33f55e30ec372e6fd6233289c14c04bb
Matheus Willian <matheuswilliandf@gmail.com>
2022-09-09 09:22:05 -0300

    criei um exemplo de menu com limpeza de tela

---
+++ projeto.py
@@ -0,0 +1,10 @@
+### Programa do projeto
+import os
+
+opcao = '1'
+while opcao != '0':
+    os.system('clear')
+    print('===========')
+    print('   Menu    ')
+    print('===========')
+    opcao = input('Opção: ')
